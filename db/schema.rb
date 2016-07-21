b59cad6b08ce35e7923bc2c0777d2ebe1e631ecf
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-07-21 04:20:19 -0400

    migrated user_id_to_blog_posts & user_id_to_comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721071413) do
+ActiveRecord::Schema.define(version: 20160721081620) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160721071413) do
     t.text     "blog_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160721071413) do
     t.text     "comment_entry"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
