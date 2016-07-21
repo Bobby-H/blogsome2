1e3aa4028b1821b93a901ff66cf6744ff2d2b854
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-07-21 02:35:27 -0400

    added cant-see-me class to comments_form & app.css

---
+++ db/migrate/20160721061944_add_value_tocomments.rb
@@ -0,0 +1,4 @@
+class AddValueTocomments < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721060748) do
+ActiveRecord::Schema.define(version: 20160721061944) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
